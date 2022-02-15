60855fee34a72aae11dd47b7cfb27c493027e9a9
Leissan Khan <leissan.khan@gmail.com>
2022-02-15 13:19:07 -0500

    commit tree c25232fc067b39d2e268e60fc44ea049a1380322

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    let yearWin=record.find(element=>element.result==="W")
+    if (yearWin){
+        return yearWin.year
+    }
+}
+  
+  superbowlWin(record)
